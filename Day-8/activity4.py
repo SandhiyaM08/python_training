0712234f6cd4fc9ff3c497c7a686386eab72c6e6
SandhiyaM08 <Sandhiya.Muthumani@Coforge.com>
2025-07-21 18:21:08 +0530

    Completed

--- Day-8/activity4.py
+++ Day-8/activity4.py
@@ -1,6 +1,20 @@
+'''#scrambled key words getting user the unscrambled word
+
+
 #assigning values
 fruit_prices={"elppa":100,"egnaro":80,"ananab":60}
 #getting input from user
-fruit_name = input("Enter fruit name: ")[::-1]
+fruit_name = input("Enter fruit name: ")
 #printing
 print(fruit_prices.get(fruit_name,{}))
+
+
+#assigning values
+fruit_prices = {"alpep": 100, "egnaro": 80, "ananab": 60}
+#getting input from user
+fruit_name = input("Enter fruit name:")
+#performing operations
+key = list(fruit_prices)[list(map(sorted, fruit_prices)).index(sorted(fruit_name))]
+print(fruit_prices[key])
+
+
